be500ae1079fdcc4dad775e471b59be636ea36cb
Ajay Bhattacharyya <33659124+AjayBhatta16@users.noreply.github.com>
2021-01-20 21:19:39 -0500

    Add files via upload

---
+++ src/head/HeadNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HeadNav() {
+  return (
+    <nav className='head-nav'>
+      <a href="#" data-section='about'>About Me</a>
+      <a href="#" data-section='projects'>My Projects</a>
+      <a href="#" data-section='contact'>Contact Info</a>
+    </nav>
+  )
+}
---
+++ src/head/HeadSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header.js'
+import HeadNav from './HeadNav.js'
+
+export default function HeadSection() {
+  return (
+    <section id='head'>
+      <Header/>
+      <HeadNav/>
+    </section>
+  )
+}
